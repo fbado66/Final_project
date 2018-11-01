3ee221555d5fa37693c946ca28233a7a6c9cc832
Franklin Bado <fbado66@gmail.com>
2018-11-01 17:13:58 -0400

    validation removed

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ApplicationRecord
   # belongs_to :user
   # optional, but probably a good idea
-   validates :external_id, :uniqueness => true
+  #  validates :external_id, :uniqueness => true
 
   # has_many :posts
   belongs_to :user
