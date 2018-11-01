fe0a8c1feb8e99d2e28c72fa8c0169fd161d99c8
Franklin Bado <fbado66@gmail.com>
2018-11-01 17:03:48 -0400

    fixing character api creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
         #  has_many :user_posts
         #  has_many :posts, through: :user_posts
 
-         has_many :characters
+        #  has_many :characters
 end
